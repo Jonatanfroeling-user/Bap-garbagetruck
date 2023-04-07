ed79a272f78b4edf7c1a5c6916c6347aa84a1f85
jonatanfroeling-user <jonafroe@student.arteveldehs.be>
2023-04-07 15:57:03 +0200

    update_20230407155703

--- Basic-implimentation/2-basic-features/5-routing/Algorithms/helpers.py
+++ Basic-implimentation/2-basic-features/5-routing/Algorithms/helpers.py
@@ -1,32 +1,64 @@
 import json, os
 from math import sqrt
 
+rawJsonFile = None
+
+with open('Basic-implimentation/data/ronse-StreetParts-276.json') as f:
+        rawJsonFile = json.load(f)
 
 # returns the raw coords of only first and last coorindate of every part, ie. waypoints
-def get_waypoints():
-    with open('Basic-implimentation/data/ronse-StreetParts-276.json') as jsonf:
-        raw = json.load(jsonf)
-        res={}
-        for street in raw.values():
-            n = []
-            for part in street['parts']:
-                first_last = part['coords'][::len(part['coords'])-1]
-                n.append({'id': part['id'], 'waypoint': [first_last]})
-            res[street['name']] = n
-        return res
+def get_waypoints(fulldata=False):
+    raw = rawJsonFile
+    res={}
+    for street in raw.values():
+        n = []
+        for part in street['parts']:
 
-# waypoints by only giving the frist coordiantes of the waypoint
+            data =  part['coords'] if fulldata else part['coords'][::len(part['coords'])-1]
+            n.append({'id': part['id'], 'waypoint': [data]})
+        res[street['name']] = n
+    return res
+
+# reversed veriosn of "get_waypoints_raw". ie. : {"[50.746604, 3.606773]": 8117600,  ....
+def get_waypoints_reversed():
+    raw = rawJsonFile
+    res={}
+    for street in raw.values():
+        for part in street['parts']:
+            res[str(part['coords'][0])] =  part['id']
+    return res
+
+# waypoints by only giving the frist coordiantes of the waypoint (limiting waypions from 800 to 100)
 def get_waypoints_raw():
-    with open('Basic-implimentation/data/ronse-StreetParts-276.json') as jsonf:
-        raw = json.load(jsonf)
-        res=[]
-        for street in raw.values():
-            for part in street['parts']:
-                res.append(part['coords'][0])
-        return res
+    raw = rawJsonFile
+    res=[]
+    for street in raw.values():
+        for part in street['parts']:
+            res.append(part['coords'][0])
+    return res
+
+# returns the parts only, {id: coords, ...}
+def get_waypoints_parts():
+    raw = rawJsonFile
+    res={}
+    for name, street in raw.items():
+        for part in street['parts']:
+            res[part['id']] =  {
+              'street': name,
+              'coords': part['coords']
+              }
+          #res[part['id']] = part['coords']
+    return res
 
 
 
+
+
+
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 def get_dis(a, b):
     return sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
